fc4da4d2aa1d21740b78ec967492050251cffa9c
Ferrolune <bergquisthampus@gmail.com>
2025-03-30 05:03:40 +0200

    moved functions

--- functions/texts.lua
+++ functions/texts.lua
@@ -21,52 +21,6 @@ function ZoneInfoTW:GetColoredLevelRange(target, table)
     return string.format(" |cff%02x%02x%02x[%s]|r", colors.Gray[1], colors.Gray[2], colors.Gray[3], "MISSING")
 end
 
--- Helper function for linear interpolation between two RGB colors
-local function InterpolateColor(c1, c2, factor)
-    local r = c1[1] + (c2[1] - c1[1]) * factor
-    local g = c1[2] + (c2[2] - c1[2]) * factor
-    local b = c1[3] + (c2[3] - c1[3]) * factor
-    return {r, g, b}
-end
-
-function ZoneInfoTW:GetLevelColor(target, table)
-    local min = table[target].low
-    local max = table[target].high
-    local playerlevel = UnitLevel("player")
-
-
-    if playerlevel < min then
-        if playerlevel <= min - 10 then
-            return ZoneInfoTW.Colors["Levels"]["Black"]
-        else
-            local factor = (playerlevel - (min - 10)) / 10
-            return InterpolateColor(ZoneInfoTW.Colors["Levels"]["Black"], ZoneInfoTW.Colors["Levels"]["Red"], factor)
-        end
-    end
-
-
-    if playerlevel <= max then
-        local mid = (min + max) / 2
-        if playerlevel <= mid then
-            local factor = (playerlevel - min) / (mid - min)
-            return
-                InterpolateColor(ZoneInfoTW.Colors["Levels"]["Orange"], ZoneInfoTW.Colors["Levels"]["Yellow"], factor)
-        else
-            local factor = (playerlevel - mid) / (max - mid)
-            return InterpolateColor(ZoneInfoTW.Colors["Levels"]["Yellow"], ZoneInfoTW.Colors["Levels"]["Green"], factor)
-        end
-    end
-
-
-    if playerlevel < max + 6 then
-        local factor = (playerlevel - max) / 6
-        return InterpolateColor(ZoneInfoTW.Colors["Levels"]["Green"], ZoneInfoTW.Colors["Levels"]["Gray"], factor)
-    else
-        return ZoneInfoTW.Colors["Levels"]["Gray"]
-    end
-end
-
-
 
 function ZoneInfoTW:GetColoredName(target, table)
     local colors = ZoneInfoTW.Colors["Factions"]
